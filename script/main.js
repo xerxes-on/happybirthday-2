d01b939b9da502887b2e8266a959a5894a421cf3
xerxes <odilbekovjavohir4@gmail.com>
2024-03-30 07:41:53 +0500

    init

--- script/main.js
+++ script/main.js
@@ -1,22 +1,4 @@
-// trigger to play music in the background with sweetalert
-window.addEventListener('load', () => {
-    Swal.fire({
-        title: 'Do you want to play music in the background?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            document.querySelector('.song').play();
-            animationTimeline();
-        } else {
-            animationTimeline();
-        }
-    });
-});
+
 
 
 // animation timeline
